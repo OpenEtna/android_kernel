797d7492435a192b8bf369fe971b6c68392ba657
polytheus <polytheus@googlemail.com>
2010-08-19 20:50:21 +0200

    Updated Android.mk to a cleaner style
    Change-Id: Ifed15a2a044db26b3f45f523c615fd0ed012fcda

--- Android.mk
+++ Android.mk
@@ -1,19 +1,19 @@
 #Android makefile to build kernel as a part of Android Build
 
 #The config for the kernel is arch/arm/configs/openetna
-
+LOCAL_PATH := $(call my-dir)
 KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
 KERNEL_CONFIG := $(KERNEL_OUT)/.config
 TARGET_PREBUILT_KERNEL := $(KERNEL_OUT)/arch/arm/boot/zImage
 
-out/target/product/openetna/kernel: $(TARGET_PREBUILT_KERNEL)
-	cp $(TARGET_PREBUILT_KERNEL) out/target/product/openetna/kernel
+$(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(transform-prebuilt-to-target)
 
 $(KERNEL_OUT):
 	mkdir -p $(KERNEL_OUT)
 
-$(KERNEL_CONFIG):
-	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- openetna
+$(KERNEL_CONFIG): $(LOCAL_PATH)/arch/arm/configs/openetna
+	$(transform-prebuilt-to-target)
 
 $(TARGET_PREBUILT_KERNEL): $(KERNEL_OUT) $(KERNEL_CONFIG)
 	$(MAKE) -j3 -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi-
