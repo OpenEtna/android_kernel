cda6714d5b9eb844891bb3835c00b33725723d86
repo sync <polytheus@googlemail.com>
2010-09-06 16:39:05 +0200

    CONFIG_LG_FW_GOOGLE_PATCH: block/genhd.c: add uevent

--- block/genhd.c
+++ block/genhd.c
@@ -977,6 +977,25 @@ static void disk_release(struct device *dev)
 	free_part_stats(&disk->part0);
 	kfree(disk);
 }
+
+/* LGE_CHANGE [dojip.kim@lge.com] 2010-04-04, google's patch */
+#if defined(CONFIG_LG_FW_GOOGLE_PATCH)
+static int disk_uevent(struct device *dev, struct kobj_uevent_env *env)
+{
+	struct gendisk *disk = dev_to_disk(dev);
+	struct disk_part_iter piter;
+	struct hd_struct *part;
+	int cnt = 0;
+
+	disk_part_iter_init(&piter, disk, 0);
+	while((part = disk_part_iter_next(&piter)))
+		cnt++;
+	disk_part_iter_exit(&piter);
+	add_uevent_var(env, "NPARTS=%u", cnt);
+	return 0;
+}
+#endif
+
 struct class block_class = {
 	.name		= "block",
 };
@@ -985,6 +1004,10 @@ static struct device_type disk_type = {
 	.name		= "disk",
 	.groups		= disk_attr_groups,
 	.release	= disk_release,
+/* LGE_CHANGE [dojip.kim@lge.com] 2010-04-04, google's patch */
+#if defined(CONFIG_LG_FW_GOOGLE_PATCH)
+	.uevent		= disk_uevent,
+#endif
 };
 
 #ifdef CONFIG_PROC_FS
