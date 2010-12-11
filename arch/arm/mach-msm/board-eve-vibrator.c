3decea4163e7b7277eb50ec30311e822a0fa7b70
Polytheus <polytheus@googlemail.com>
2010-12-11 17:23:59 +0100

    board-eve-vibrator: cleanup
    Change-Id: If0f5ad9c38cbd05c37f0e8bbf7756fbe29c1c603

--- arch/arm/mach-msm/board-eve-vibrator.c
+++ arch/arm/mach-msm/board-eve-vibrator.c
@@ -3,32 +3,30 @@
  * (C) 2008 LGE, Inc.
  * (C) 2007 Google, Inc.
  *
- * Android vibrator driver 
+ * Android vibrator driver
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
-*/
+ */
 
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/timer.h>
 #include <mach/gpio.h>
-#include <linux/workqueue.h>  
-#include <linux/spinlock.h> //spinlock for variable of  &s_amp
+#include <linux/workqueue.h>
+#include <linux/spinlock.h>
 #include <asm/io.h>
 #include <mach/msm_iomap.h>
 #include <mach/vreg.h>
-/*++ ViBrator setting *///diyu@lge.com
 #include <linux/i2c.h>
 #include <../../../drivers/staging/android/timed_output.h>
 #include "board-eve.h"
 
-#define TRUE 		1
-#define FALSE 		0
-#define I2C_NO_REG 	0xFF
+#define TRUE		1
+#define FALSE		0
+#define I2C_NO_REG	0xFF
 
-/*-- ViBrator setting *///diyu@lge.com
 
 #define VIBRATOR_DEBUG 0
 #if VIBRATOR_DEBUG
@@ -70,52 +68,49 @@ static atomic_t s_amp = ATOMIC_INIT(1);
 static int s_vibstate = 0;
 static atomic_t s_vibstate_work_check =  ATOMIC_INIT(0);
 static uint8_t vibrator_start_first = FALSE;
-//static int s_vibstate_work = 0; /*0: not to work, 1: working on*/
-
 
 #define MOTOR_VOLT_LEVEL	3000 /* 3000mV */
-#define LDO_NO_MOTOR 		2
+#define LDO_NO_MOTOR		2
 
 int bd6083_set_ldo_power(int ldo_no, int on);
 int bd6083_set_ldo_vout(int ldo_no, unsigned vout_mv);
 
 int bd6083_ldo_poweron(void)
 {
-    if (bd6083_set_ldo_power(LDO_NO_MOTOR, 1)) {
-         printk("android_vibrator: failed to set motor poweron\n");
-         return -1;
-    }
-    return 0;
+	if (bd6083_set_ldo_power(LDO_NO_MOTOR, 1)) {
+		printk("android_vibrator: failed to set motor poweron\n");
+		return -1;
+	}
+	return 0;
 }
 
 int bd6083_ldo_poweroff(void)
 {
-    if (bd6083_set_ldo_power(LDO_NO_MOTOR, 0)) {
-        printk("android_vibrator: failed to set motor poweroff\n");
-        return -1;
-    }
-    return 0;
+	if (bd6083_set_ldo_power(LDO_NO_MOTOR, 0)) {
+		printk("android_vibrator: failed to set motor poweroff\n");
+		return -1;
+	}
+	return 0;
 }
 
 static int vibrator_motor_power_switch(int on)
 {
-    if (on) 
-	    return bd6083_ldo_poweron();
-    else
-        return bd6083_ldo_poweroff();
+	if (on)
+		return bd6083_ldo_poweron();
+	else
+		return bd6083_ldo_poweroff();
 }
 
 static int vibrator_motor_set_level(int mvolt)
 {
-    if (bd6083_set_ldo_vout(LDO_NO_MOTOR, mvolt)) {
-        printk("android_vibrator: failed to set motor poweron\n");
-        return -1;
-    }
-    return 0;
+	if (bd6083_set_ldo_vout(LDO_NO_MOTOR, mvolt)) {
+		printk("android_vibrator: failed to set motor poweron\n");
+		return -1;
+	}
+	return 0;
 }
 
-//static //LGE_CHANGE [antispoon@lge.com,diyu@lge.com] 2009-07-17 comment out "static" for AT+MOT 
-void vibrator_disable(void)
+static void vibrator_disable(void)
 {
 
 	gpio_set_value(GPIO_LIN_MOTOR_EN, 0);
@@ -126,8 +121,7 @@ void vibrator_disable(void)
 	s_vibstate = 0;
 }
 
-//static //LGE_CHANGE [antispoon@lge.com,diyu@lge.com] 2009-07-17 comment out "static" for AT+MOT 
-void vibrator_enable(void)
+static void vibrator_enable(void)
 {
 
 	REG_WRITEL((GPMN_M_DEFAULT & GPMN_M_MASK), GP_MN_CLK_MDIV);
@@ -135,18 +129,17 @@ void vibrator_enable(void)
 
 	gpio_set_value(GPIO_LIN_MOTOR_EN, 1);
 	gpio_set_value(GPIO_LIN_MOTOR_PWM, 1);
-	
+
 	vibrator_motor_power_switch(1);
 
 	s_vibstate = 1;
 }
 
-//static //LGE_CHANGE [antispoon@lge.com,diyu@lge.com] 2009-07-17 comment out "static" for AT+MOT 
-void vibrator_set(int amp)
+static void vibrator_set(int amp)
 {
 	int gain;
 
-	printk(KERN_ERR "android-vibrator: vibrator_set amp=%d\n",amp);
+	VDBG("android-vibrator: vibrator_set amp=%d\n",amp);
 
 	if(amp == 0) {
 		if(s_vibstate)
@@ -158,16 +151,15 @@ void vibrator_set(int amp)
 
 		if(!vibrator_start_first){
 			vibrator_motor_set_level(MOTOR_VOLT_LEVEL);
-			vibrator_start_first = TRUE; 
+			vibrator_start_first = TRUE;
 		}
-        
+
 		gain = ((PWM_MULTIPLIER * amp) >> 8) + GPMN_D_DEFAULT ;
 		REG_WRITEL((gain & GPMN_D_MASK), GP_MN_CLK_DUTY);
 	}
 }
 
-//LGE_CHANGE [antispoon@lge.com,diyu@lge.com] 2009-07-17 for AT+MOT
-int is_vib_state(void) //antispoon0713 diyu@lge.com
+static int is_vib_state(void)
 {
 	return s_vibstate;
 }
@@ -177,245 +169,69 @@ static void vibrator_work_func(struct work_struct *work)
 	vibrator_set(0);
 }
 
-static void vibrator_timeout(unsigned long arg __attribute__((unused))) 
+static void vibrator_timeout(unsigned long arg __attribute__((unused)))
 {
-//	schedule_work(&vibrator_work);
 	vibrator_set(0);
 	atomic_set(&s_vibstate_work_check ,0);
-//	s_vibstate_work =0;
 }
 
-#if 0
-static ssize_t vibrator_enable_show(struct device *dev, struct device_attribute *attr, char *buf)
-{
-	int timeout = atomic_read(&s_vibrator);
-	return sprintf(buf, "%d\n", JIFFIES_TO_MS(timeout));
-}
+static void to_vibrator_enable(struct timed_output_dev *sdev, int timeout) {
 
-spinlock_t vibrator_lock;
+	VDBG("vibrator_enable(%d)\n",timeout);
+	atomic_set(&s_amp, 120);
+	atomic_set(&s_vibrator, timeout);
 
-static ssize_t vibrator_enable_store(
-		struct device *dev, struct device_attribute *attr, 
-		const char *buf, size_t size)
-{
-	int value;
-	sscanf(buf, "%d", &value);
-
-	/* LGE_CHANGE [dojip.kim@lge.com] 2010-05-13, fixed the lockup issue:
-	 * not use the spinclock
-	 */
-	//spin_lock(&vibrator_lock);
-	atomic_set(&s_vibrator, value);
-
-	/* 
+	/*
 	 * value == -1 : infinite vibration
-	 * value == 0 : stop vibration	 
+	 * value == 0 : stop vibration
 	 * value > 0 : vibrate during <value> miliseconds
 	 */
-	 VDBG("%s -1",__FUNCTION__);
 	if(atomic_read(&s_vibrator)>0){
 		if (atomic_read(&s_vibstate_work_check) ==0){
 			del_timer(&s_timer);
-			VDBG("%s -2",__FUNCTION__);
 			vibrator_set(atomic_read(&s_amp));
 			s_timer.expires = jiffies + MS_TO_JIFFIES(atomic_read(&s_vibrator));
-//			s_timer.expires = jiffies + MS_TO_JIFFIES(value);
 			add_timer(&s_timer);
 			atomic_set(&s_vibstate_work_check ,1);
-//			s_vibstate_work =1;
 		}
 	}
-	else if ( atomic_read(&s_vibrator) == 0){		
+	else if ( atomic_read(&s_vibrator) == 0){
 		if (atomic_read(&s_vibstate_work_check) ==1){
 			del_timer(&s_timer);
-			VDBG("%s -3",__FUNCTION__);
-			
+
 			vibrator_set(0);
 			atomic_set(&s_vibstate_work_check ,0);
-//			s_vibstate_work =0;
 		}
 	}
-	/* LGE_CHANGE [dojip.kim@lge.com] 2010-05-13, fixed the lockup issue:
-	 * not use the spinclock
-	 */
-	//spin_unlock(&vibrator_lock);
-
-	return size;
-}
-static ssize_t vibrator_amp_show(struct device *dev, struct device_attribute *attr, char *buf)
-{
-	int amp = atomic_read(&s_amp);
-	return sprintf(buf, "%d\n", amp);
-}
-
-static ssize_t vibrator_amp_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t size)
-{
-	int value;
-	VDBG("%s -2",__FUNCTION__);
-
-	sscanf(buf, "%d", &value);
-	atomic_set(&s_amp, value);
-
-	return size;
-}
-
-static DEVICE_ATTR(enable, S_IRUGO | S_IWUSR, vibrator_enable_show, vibrator_enable_store);
-static DEVICE_ATTR(amp, S_IRUGO | S_IWUSR, vibrator_amp_show, vibrator_amp_store);
-
-static int  android_vibrator_suspend(struct platform_device *pdev, pm_message_t state)
-{
-	/* future capability*/
-	//LGE_CHANGE [antispoon@lge.com,diyu@lge.com] 2009-10-24 Add Voltage Setting one more
-	vibrator_start_first = FALSE; 
-	printk("%s()\n",__FUNCTION__);
-	vibrator_disable();
-	return 0;
-}
-
-static int  android_vibrator_resume(struct platform_device *pdev)
-{
-	/* future capability*/
-	return 0;
-}
-
-static int android_vibrator_probe(struct platform_device *pdev)
-{
-	int err;
-	VDBG( "%s -1 Fail\n",__FUNCTION__);
-
-	s_vibrator_gpio = (int)pdev->dev.platform_data;
-	err = device_create_file(&pdev->dev, &dev_attr_enable);
-	if (err) {
-		printk( "android-vibrator: vibrator_probe: Fail\n");
-		return err;
-	}
-	
-	VDBG( "%s -2 Fail\n",__FUNCTION__);
-	err = device_create_file(&pdev->dev, &dev_attr_amp);
-	if (err) {
-		printk( "android-vibrator: vibrator_probe: Fail\n");
-		device_remove_file(&pdev->dev, &dev_attr_enable);
-		return err;
-	}
-	
-	VDBG( "%s -3 Fail\n",__FUNCTION__);
-	gpio_request(GPIO_LIN_MOTOR_EN, "gpio_lin_motor_en");
-	gpio_request(GPIO_LIN_MOTOR_PWM, "gpio_lin_motor_pwm");
-
-	/* Off Enable */
-	gpio_set_value(GPIO_LIN_MOTOR_EN, 0);
-	
-	VDBG( "%s -4 Fail\n",__FUNCTION__);
-
-	/* Set PWM frequency */
-	#if 1
-	REG_WRITEL((GPMN_M_DEFAULT & GPMN_M_MASK), GP_MN_CLK_MDIV);
-	REG_WRITEL((~( GPMN_N_DEFAULT - GPMN_M_DEFAULT )&GPMN_N_MASK), GP_MN_CLK_NDIV);
-	gpio_set_value(GPIO_LIN_MOTOR_PWM, 0);
-	#endif //temp
-	VDBG( "%s -5 Fail\n",__FUNCTION__);
-
-        /* XXX: this will be done in the charge pump driver
-         * because of the kernel panic */
-	//vibrator_motor_set_level(MOTOR_VOLT_LEVEL);
-	VDBG( "%s -6 Fail\n",__FUNCTION__);
-
-	/* LGE_CHANGE [dojip.kim@lge.com] 2010-05-13, fixed the lockup issue:
-	 * not use the spinclock
-	 */
-	//spin_lock_init(&vibrator_lock);
-
-	printk(KERN_ERR "android-vibrator: vibrator_probe: Done\n");
-	
-	return 0;
-}
-
-static int android_vibrator_remove(struct platform_device *pdev) 
-{
-	device_remove_file(&pdev->dev, &dev_attr_enable);
-	device_remove_file(&pdev->dev, &dev_attr_amp);
-
-	REG_WRITEL((GPMN_M_DEFAULT & GPMN_M_MASK), GP_MN_CLK_MDIV);
-	REG_WRITEL((~( GPMN_N_DEFAULT - GPMN_M_DEFAULT )&GPMN_N_MASK), GP_MN_CLK_NDIV);
-	REG_WRITEL((GPMN_D_DEFAULT & GPMN_D_MASK), GP_MN_CLK_DUTY);
-	vibrator_motor_power_switch(0);
-	vibrator_motor_set_level(0);
-
-	return 0;
-}
-
-static struct platform_driver android_vibrator_driver = {
-	.probe		= android_vibrator_probe,
-	.remove		= android_vibrator_remove,
-	.suspend	= android_vibrator_suspend,
-	.resume		= android_vibrator_resume,
-	.driver		= {
-		.name		= "android-vibrator",
-		.owner		= THIS_MODULE,
-	},
-};
-#endif
-
-static void to_vibrator_enable(struct timed_output_dev *sdev, int timeout) {
-    printk("vibrator_enable(%d)\n",timeout);
-    atomic_set(&s_amp, 120);
-    atomic_set(&s_vibrator, timeout);
-
-    /* 
-     * value == -1 : infinite vibration
-     * value == 0 : stop vibration   
-     * value > 0 : vibrate during <value> miliseconds
-     */
-     VDBG("%s -1",__FUNCTION__);
-    if(atomic_read(&s_vibrator)>0){
-        if (atomic_read(&s_vibstate_work_check) ==0){
-            del_timer(&s_timer);
-            VDBG("%s -2",__FUNCTION__);
-            vibrator_set(atomic_read(&s_amp));
-            s_timer.expires = jiffies + MS_TO_JIFFIES(atomic_read(&s_vibrator));
-            add_timer(&s_timer);
-            atomic_set(&s_vibstate_work_check ,1);
-        }
-    }
-    else if ( atomic_read(&s_vibrator) == 0){
-        if (atomic_read(&s_vibstate_work_check) ==1){
-            del_timer(&s_timer);
-            VDBG("%s -3",__FUNCTION__);
-
-            vibrator_set(0);
-            atomic_set(&s_vibstate_work_check ,0);
-        }
-    }
 }
 
 static int to_vibrator_get_time(struct timed_output_dev *sdev) {
-  printk("vibrator_get_time\n");
-  return atomic_read(&s_vibrator);
+	VDBG("vibrator_get_time\n");
+	return atomic_read(&s_vibrator);
 }
 
 static struct timed_output_dev pmic_vibrator = {
-    .name = "vibrator",
-    .get_time = to_vibrator_get_time,
-    .enable = to_vibrator_enable,
+	.name = "vibrator",
+	.get_time = to_vibrator_get_time,
+	.enable = to_vibrator_enable,
 };
 
 
 static int __init android_vibrator_init(void)
 {
 	printk( "android-vibrator: init\n");
-    
-    gpio_request(GPIO_LIN_MOTOR_EN, "gpio_lin_motor_en");
-    gpio_request(GPIO_LIN_MOTOR_PWM, "gpio_lin_motor_pwm");
 
-    /* Off Enable */
-    gpio_set_value(GPIO_LIN_MOTOR_EN, 0);
+	gpio_request(GPIO_LIN_MOTOR_EN, "gpio_lin_motor_en");
+	gpio_request(GPIO_LIN_MOTOR_PWM, "gpio_lin_motor_pwm");
 
-    REG_WRITEL((GPMN_M_DEFAULT & GPMN_M_MASK), GP_MN_CLK_MDIV);
-    REG_WRITEL((~( GPMN_N_DEFAULT - GPMN_M_DEFAULT )&GPMN_N_MASK), GP_MN_CLK_NDIV);
-    gpio_set_value(GPIO_LIN_MOTOR_PWM, 0);
+	/* Off Enable */
+	gpio_set_value(GPIO_LIN_MOTOR_EN, 0);
 
-    printk("android_vibrator_init done\n");
-    return timed_output_dev_register(&pmic_vibrator);
+	REG_WRITEL((GPMN_M_DEFAULT & GPMN_M_MASK), GP_MN_CLK_MDIV);
+	REG_WRITEL((~( GPMN_N_DEFAULT - GPMN_M_DEFAULT )&GPMN_N_MASK), GP_MN_CLK_NDIV);
+	gpio_set_value(GPIO_LIN_MOTOR_PWM, 0);
+
+	return timed_output_dev_register(&pmic_vibrator);
 }
 
 module_init(android_vibrator_init);
